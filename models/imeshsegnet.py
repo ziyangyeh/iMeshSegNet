d1c7e75b41edff2fd5b0166b48b49a49b80b9364
Ziyang Ye <yeziyang159@gmail.com>
2022-08-22 15:29:37 +0800

    implementation of iMeshSegNet

--- models/imeshsegnet.py
+++ models/imeshsegnet.py
@@ -19,10 +19,12 @@ class iMeshSegNet(nn.Module):
         # FTM (feature-transformer module)
         self.fstn = STNkd(k=64)
         # GLM-1 (graph-contrained learning modulus)
-        self.glm1_conv1_1 = torch.nn.Conv1d(64, 32, 1)
-        self.glm1_conv1_2 = torch.nn.Conv1d(64, 32, 1)
-        self.glm1_bn1_1 = nn.BatchNorm1d(32)
-        self.glm1_bn1_2 = nn.BatchNorm1d(32)
+        self.glm1_conv1 = torch.nn.Conv1d(64, 32, 1)
+        self.glm1_conv2_1 = torch.nn.Conv2d(3*2, 64, 1, bias=False)
+        self.glm1_conv2_2 = torch.nn.Conv2d(64, 32, 1, bias=False)
+        self.glm1_bn1 = nn.BatchNorm1d(32)
+        self.glm1_bn2_1 = nn.BatchNorm2d(64)
+        self.glm1_bn2_2 = nn.BatchNorm2d(32)
         self.glm1_conv2 = torch.nn.Conv1d(32+32, 64, 1)
         self.glm1_bn2 = nn.BatchNorm1d(64)
         # MLP-2
@@ -33,14 +35,18 @@ class iMeshSegNet(nn.Module):
         self.mlp2_conv3 = torch.nn.Conv1d(128, 512, 1)
         self.mlp2_bn3 = nn.BatchNorm1d(512)
         # GLM-2 (graph-contrained learning modulus)
-        self.glm2_conv1_1 = torch.nn.Conv1d(512, 128, 1)
-        self.glm2_conv1_2 = torch.nn.Conv1d(512, 128, 1)
-        self.glm2_conv1_3 = torch.nn.Conv1d(512, 128, 1)
-        self.glm2_bn1_1 = nn.BatchNorm1d(128)
-        self.glm2_bn1_2 = nn.BatchNorm1d(128)
-        self.glm2_bn1_3 = nn.BatchNorm1d(128)
-        self.glm2_conv2 = torch.nn.Conv1d(128*3, 512, 1)
-        self.glm2_bn2 = nn.BatchNorm1d(512)
+        self.glm2_conv1 = torch.nn.Conv1d(512, 128, 1)
+        self.glm2_conv2_1 = torch.nn.Conv2d(3*2, 256, 1, bias=False)
+        self.glm2_conv2_2 = torch.nn.Conv2d(256, 128, 1, bias=False)
+        self.glm2_conv3_1 = torch.nn.Conv2d(3*2, 256, 1, bias=False)
+        self.glm2_conv3_2 = torch.nn.Conv2d(256, 128, 1, bias=False)
+        self.glm2_bn1 = nn.BatchNorm1d(128)
+        self.glm2_bn2_1 = nn.BatchNorm2d(256)
+        self.glm2_bn2_2 = nn.BatchNorm2d(128)
+        self.glm2_bn3_1 = nn.BatchNorm2d(256)
+        self.glm2_bn3_2 = nn.BatchNorm2d(128)
+        self.glm2_conv4 = torch.nn.Conv1d(128*3, 512, 1)
+        self.glm2_bn4 = nn.BatchNorm1d(512)
         # MLP-3
         self.mlp3_conv1 = torch.nn.Conv1d(64+512+512+512, 256, 1)
         self.mlp3_conv2 = torch.nn.Conv1d(256, 256, 1)
@@ -55,7 +61,7 @@ class iMeshSegNet(nn.Module):
         if self.with_dropout:
             self.dropout = nn.Dropout(p=self.dropout_p)
 
-    def forward(self, x, a_s, a_l):
+    def forward(self, x, kg_6, kg_12):
         batchsize = x.size()[0]
         n_pts = x.size()[2]
         # MLP-1
@@ -66,12 +72,13 @@ class iMeshSegNet(nn.Module):
         x = x.transpose(2, 1)
         x_ftm = torch.bmm(x, trans_feat)
         # GLM-1
-        sap = torch.bmm(a_s, x_ftm)
-        sap = sap.transpose(2, 1)
         x_ftm = x_ftm.transpose(2, 1)
-        x = F.relu(self.glm1_bn1_1(self.glm1_conv1_1(x_ftm)))
-        glm_1_sap = F.relu(self.glm1_bn1_2(self.glm1_conv1_2(sap)))
-        x = torch.cat([x, glm_1_sap], dim=1)
+        x = F.relu(self.glm1_bn1(self.glm1_conv1(x_ftm)))
+        ec12 = self.glm1_bn2_1(self.glm1_conv2_1(kg_12))
+        ec12 = self.glm1_bn2_2(self.glm1_conv2_2(ec12))
+        ec12 = F.leaky_relu(ec12, negative_slope=0.2)
+        ec12 = ec12.max(dim=-1, keepdim=False)[0] 
+        x = torch.cat([x, ec12], dim=1)
         x = F.relu(self.glm1_bn2(self.glm1_conv2(x)))
         # MLP-2
         x = F.relu(self.mlp2_bn1(self.mlp2_conv1(x)))
@@ -80,17 +87,17 @@ class iMeshSegNet(nn.Module):
         if self.with_dropout:
             x_mlp2 = self.dropout(x_mlp2)
         # GLM-2
-        x_mlp2 = x_mlp2.transpose(2, 1)
-        sap_1 = torch.bmm(a_s, x_mlp2)
-        sap_2 = torch.bmm(a_l, x_mlp2)
-        x_mlp2 = x_mlp2.transpose(2, 1)
-        sap_1 = sap_1.transpose(2, 1)
-        sap_2 = sap_2.transpose(2, 1)
-        x = F.relu(self.glm2_bn1_1(self.glm2_conv1_1(x_mlp2)))
-        glm_2_sap_1 = F.relu(self.glm2_bn1_2(self.glm2_conv1_2(sap_1)))
-        glm_2_sap_2 = F.relu(self.glm2_bn1_3(self.glm2_conv1_3(sap_2)))
-        x = torch.cat([x, glm_2_sap_1, glm_2_sap_2], dim=1)
-        x_glm2 = F.relu(self.glm2_bn2(self.glm2_conv2(x)))
+        x = F.relu(self.glm2_bn1(self.glm2_conv1(x_mlp2)))
+        ec12 = self.glm2_bn2_1(self.glm2_conv2_1(kg_12))
+        ec12 = self.glm2_bn2_2(self.glm2_conv2_2(ec12))
+        ec12 = F.leaky_relu(ec12, negative_slope=0.2)
+        ec12 = ec12.max(dim=-1, keepdim=False)[0] 
+        ec6 = self.glm2_bn3_1(self.glm2_conv3_1(kg_6))
+        ec6 = self.glm2_bn3_2(self.glm2_conv3_2(ec6))
+        ec6 = F.leaky_relu(ec6, negative_slope=0.2)
+        ec6 = ec6.max(dim=-1, keepdim=False)[0] 
+        x = torch.cat([x, ec12, ec6], dim=1)
+        x_glm2 = F.relu(self.glm2_bn4(self.glm2_conv4(x)))
         # GMP
         x = torch.max(x_glm2, 2, keepdim=True)[0]
         # Upsample
